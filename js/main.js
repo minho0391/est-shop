4a1de82f5b54cdc1b2baab06fe8f465e6924ea0c
minho0391 <minh05@naver.com>
2026-06-03 12:41:46 +0900

    feat: display product list using fetch

--- js/main.js
+++ js/main.js
@@ -5,6 +5,41 @@ async function fetchProducts() {
     const data = await res.json();
     console.log(data.products);
     console.log(data.products.slice(0, 12));
+    const products = data.products.slice(0, 12);
+    /*
+    빈 fragment를 생성
+    products의 마다 할일
+    빈 요소에 12개의 article을 생성하고,
+    product-grid에 fragment의 내용을 html 태그로 생성
+*/
+    const productGrid = document.querySelector(".product-grid");
+    const fragment = document.createDocumentFragment();
+
+    products.forEach(product => {
+      const article = document.createElement("article");
+
+      article.classList.add("product-card");
+
+      article.innerHTML = `
+        <img src="${product.thumbnail}" alt="${product.title}">
+        <div class="product-info">
+          <h3>${product.title}</h3>
+          <p>${product.brand}</p>
+          <div class="product-bottom">
+            <strong>${product.price}$</strong>
+            <button
+              type="button"
+              class="cart-add"
+              aria-label="${product.title} 장바구니 담기">
+            </button>
+          </div>
+        </div>
+      `;
+
+      fragment.append(article);
+    });
+
+    productGrid.append(fragment);
   } catch {
   } finally {
   }
